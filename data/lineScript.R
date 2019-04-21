a122362adcda2b7fdb1c6d2ba4a2bb1d597690c2
benmatheson <mathesonben@gmail.com>
2019-04-21 11:59:28 -0800

    smaller

--- data/lineScript.R
+++ data/lineScript.R
@@ -1,24 +1,47 @@
 library(tidyverse)
 library(rvest)
 library(lubridate)
+library(jsonlite)
 setwd("projects/iditarod2019/data")
 
 
 getwd()
 
 res <- read_csv("data19Complete.csv")
-checkLoc <- read_csv("data19Complete.csv")
+checkLoc <- read_csv("checkpoints.csv")
 
 View(res)
 
 resGroup <- res %>% group_by(musher, checkName) %>% summarise (checkPlace = cpOrder)
-resGroup
+resGroupJoined <- left_join(resGroup, checkLoc, by=(c("checkName"="location")))
+
+resGroupJoined <- resGroupJoined %>% select(1:4)
+View(resGroupJoined)
+
+write_csv(resGroupJoined, "resGroup.csv")
+View(checkLoc)
 
 
-write_csv(resGroup, "resGroup.csv")
-View(resGroup)
+
+uniqueMushers <- unique(res$musher)
+
+write_csv(as.data.frame(uniqueMushers), "uniqueMushers.csv")
 
 
+musherArray <- toJSON(uniqueMushers)
+\
+
+
+### this only selects the hours.
+
+
+all <-  read_csv("allData.csv")
+
+allSmall <- grepl("00:00", all$currentTimeStamp, value=T  )
+allHour <- all %>% filter (grepl("00:00", currentTimeStamp  ))
+write_csv(allHour,"allHour.csv")
+
+View(allSmall)
 
 # need to add cp index here somehow
 
--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ console.log(units+"units");
 
 
 
-const lineScaleX = d3.scaleLinear().domain([53,1]).range([0,units-10])
+const lineScaleX = d3.scaleLinear().domain([53,1]).range([90,units-10])
 const lineScaleY = d3.scaleLinear().domain([0,23]).range([6,yUnits-60])
 
 const lineGenerator =  d3.line()
@@ -79,8 +79,9 @@ var formatStamp = d3.timeFormat("%A, %B %e %H:%M")
 // parseTime("June 30, 2015"); // Tue Jun 30 2015 00:00:00 GMT-0700 (PDT)
 
 
-const timesMini = times.filter((d,i)=> i%4 ==0)
-
+// const timesMini = times.filter((d,i)=> i%10 ==0)
+const timesMini = times.filter((d,i)=> i%12 ==0)
+console.log(timesMini)
 
 // const timesSmall = times.filter(d=> d>'2019-03-04 12:10:00'&&  d>'2019-03-04 19:10:00')
 
@@ -92,10 +93,11 @@ const timesMini = times.filter((d,i)=> i%4 ==0)
 
 
 
-    d3.csv("data/allData.csv", function(data){
+    d3.csv("data/allHour.csv", function(data){
         d3.csv("data/locationIndexName.csv",function(labelData) {
             d3.csv("data/resGroup.csv", function (lineData){
 
+                const labelDataSelect = labelData.filter((d,i)=> i%2 == 0)
 
         document.querySelector(".replay").addEventListener("click", loop)
         // function full (){
@@ -181,7 +183,8 @@ const timesMini = times.filter((d,i)=> i%4 ==0)
         
             const svg = d3.select("#dot").append('svg').attr("height",height).attr("width", width).attr("class", "bg").attr("id", "dotSvg")
             const svgG = svg.append('g')
-            const svgLabel = svg.append('g');
+            // const svgLabel = svg.append('g');
+            
 
 
             d3.selectAll('p').remove();
@@ -204,15 +207,16 @@ const timesMini = times.filter((d,i)=> i%4 ==0)
             // const textArea = svgTop.append('g')
             const textArea = d3.select('#time').append('p')
             
-            
+        
+
             const svg1 = svgG.append('g')
             const textY = 60
             
             
-            const labelDataSelect = labelData.filter((d,i)=> i%2 == 0)
        
             
-            
+            const svgLabel = svg.append('g');
+
             svgLabel.selectAll('text').data(labelDataSelect).enter().append('text')
                 .attr("x", textY+leftOffset)
                 .attr('y', (d,i) =>  (i*21) +30)
@@ -221,7 +225,7 @@ const timesMini = times.filter((d,i)=> i%4 ==0)
                 .attr('class', "city")
             
             
-            
+            // highlight("LanceMackey")
 
 
 
@@ -232,85 +236,51 @@ const timesMini = times.filter((d,i)=> i%4 ==0)
 
 
 
+        const pete1 = d3.select("#PeterKaiser").attr("stroke-width", 4).attr("stroke", "salmon")
 
 
             
             timesMini.forEach(function(t, i){
-        // console.log(i)
-                if (i <= times.length-1) {
-        setTimeout(function inner (){
-        console.log(i)
-            // console.log("looping")
-            const currentData = data.filter(d=> d.parsedDate == t.toString())
-            // console.log(currentData)
+
+                function inner (){
+                    // console.log("looping")
+                    const currentData = data.filter(d=> d.parsedDate == t.toString())
+                    // console.log(currentData)
         
-        
-        //  svg1.selectAll('text')
-        //   .data(currentData)
-        //   .enter()
-        //   .append('text')
-        //   .attr("x", d=>scaleX(d.index))
-        //   .attr('y', (d,i)=>500-(d.relOrder*8))
-        //   .text(d=>d.currentMusher)
-        
-        
-        
-        const newRects = svgG.selectAll('rect').data(currentData, d=>d.currentMusher)
-        // newRects.exit().remove();
-        
-        
-        
-        newRects.transition()
-            .duration(300)
-            // newRects.enter().append('rect')
-            // .attr("width", 8)
-            // .attr("height", 8)
-            // .attr("height", d=> d.index%2 > 0 ? 7 : 5)
-            // .attr("width", d=> d.index%2 > 0 ? 7 : 5)
-            .attr("y", d=>scaleX(d.index)+13)
-            .attr('x', (d,i)=>(d.relOrder*10+leftOffset+60))
-            // .attr('x', 30)
-            // .attr("width", d=> d.index%2 > 0 ? 4 : 7)
-            // .attr("height", d=> d.index%2 > 0 ? 4 : 7)
-            // .attr("fill", d=> d.index%2 > 0 ? "#333" : "purple")
-            .attr("opacity", d=> d.index%2 > 0 ? .3 : 1)
-            // .attr("stroke", "black")
-            // .attr("rx", 4)
-            // .attr("ry", 4)
-            // .attr("stroke", 'white')
-            // .attr("fill", '#444')
-            .ease(d3.easeBounce)
-            .ease(d3.easeLinear)
-            
-            newRects.append('text').text(d=>d.currentMusher).attr('x', 0).attr('y', 1)
-        
-        
-        
-        // const pete1 = d3.select("#PeterKaiser").attr("fill", "salmon")
-        // const nic1 = d3.select("#NicolasPetit").attr("fill", "green")
-        // const joar1 = d3.select("#JoarLeifsethUlsom").attr("fill", "blue")
-        
-        textArea.selectAll('text').remove()
-        
-        textArea.append('text').attr('x', 0).attr('y', 0).text(formatStamp(parseTime(t))).attr("class", "time")
-        // textArea.append('rect').attr('x', 200).attr('y', 150).attr("width", 330).attr("height", 20).attr("fill", "salmon")
-        // textArea.append('rect').attr('x', 500).attr('y', 200).attr("width", 100).attr("height", 100).attr("fill", "blue")
-        // svg.selectAll('g').remove()
-        
-        // console.log(timeStamp)
-        // console.log(t)
-        // const unk = textArea.append('p').text("Unalakleet").attr("class", "city")
-        
-        
-            },300*i)
+                
+    
+                const newRects = svgG.selectAll('rect').data(currentData, d=>d.currentMusher)
+                
+                newRects.transition()
+                    .duration(430)
+                    .attr("y", d=>scaleX(d.index)+13)
+                    .attr('x', (d,i)=>(d.relOrder*10+leftOffset+60))
+                    .attr("opacity", d=> d.index%2 > 0 ? .3 : 1)
+                        .ease(d3.easeLinear)
+                    
+                    newRects.append('text').text(d=>d.currentMusher).attr('x', 0).attr('y', 1)
+                
+                
+                
+                
+                textArea.selectAll('text').remove()
+                
+                textArea.append('text').attr('x', 0).attr('y', 0).text(formatStamp(parseTime(t))).attr("class", "time")
+              
+                
+                    }
+
+
+                if (i <= timesMini.length) {
+        setTimeout(inner,400*i)
              } else  {
                  
                 console.log("DDdone??");
-                clearInterval(inner)
+                clearTimeout(inner)
             }
             
             
-            
+    
             })
         
             } //loop
@@ -347,6 +317,23 @@ lineData.forEach(function (r){
         //append svg
     const svgLine = d3.select("#line").append("svg").attr("width", units).attr("height", yUnits)
     
+
+
+    const svgLabelLine = svgLine.append('g');
+
+    svgLabelLine.selectAll('text').data(labelDataSelect).enter().append('text')
+        .attr("x", leftOffset+55)
+        .attr('y', (d,i) =>  (i*21) +30)
+        .text(d=> `${d.location_name}`)
+        .attr('text-anchor', "end")
+        .attr('class', "citySmall")
+
+
+
+
+
+
+
 
 
 
