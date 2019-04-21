6b7d584c17878ad282705089588f8cc059155bb7
benmatheson <mathesonben@gmail.com>
2019-04-20 18:25:03 -0800

    function

--- js/main.js
+++ js/main.js
@@ -8,11 +8,25 @@ const yUnits = 550;
 const screenWidth = window.innerWidth;
 
 var units;
-screenWidth < 500 ? units = 400 : units= (screenWidth -50)/2;
+screenWidth < 500 ? units = 400 : units= (screenWidth -30)/2;
 console.log(units+"units");
 
-console.log(screenWidth)
-console.log("screenWidth")
+
+
+
+
+
+
+
+const lineScaleX = d3.scaleLinear().domain([53,0]).range([0,units-10])
+const lineScaleY = d3.scaleLinear().domain([0,23]).range([6,yUnits-35])
+
+const lineGenerator =  d3.line()
+                    .x(d=> lineScaleX(d.checkPlace))
+                    .y(d=>lineScaleY(d.index))
+                    .curve(d3.curveNatural) 
+                    .curve(d3.curveBundle) 
+                    .curve(d3.curveStep) 
 
 function highlight (musher){
 
@@ -27,7 +41,7 @@ function highlight (musher){
     
 const musherParsed =musher.replace(" ","").replace(" ", "");
 
-    console.log("this is the"+ musherParsed)
+    // console.log("this is the"+ musherParsed)
 
 
 
@@ -61,36 +75,39 @@ var formatStamp = d3.timeFormat("%A, %B %e %H:%M")
 // const timesSmall = times.filter(d=> d>'2019-03-04 12:10:00'&&  d>'2019-03-04 19:10:00')
 
 
-function full (){
-
-    d3.selectAll('svg').remove();
-    d3.selectAll('p').remove();
-
-const height = 550;
-// const width= 700;
-const width = units;
-
-
-const svg = d3.select("#dot").append('svg').attr("height",height).attr("width", width).attr("class", "bg")
-const svgG = svg.append('g')
-const svgLabel = svg.append('g');
-
-// const svgLine = d3.select("#line").append("svg").attr("width", units).attr("height", yUnits)
-
-
-    console.log("this is the full ")
 
     // d3.selectAll('rect').remove();
     // d3.selectAll('text').remove();
 
+
+
+
     d3.csv("data/allData.csv", function(data){
         d3.csv("data/locationIndexName.csv",function(labelData) {
+            d3.csv("data/resGroup.csv", function (lineData){
+
+
+        document.querySelector(".replay").addEventListener("click", full)
+        function full (){
+
+            console.log("is this full!!")
+                    d3.selectAll('svg').remove();
+                    d3.selectAll('p').remove();
+                
+                const height = 550;
+                // const width= 700;
+                const width = units;
+                
+                
+                const svg = d3.select("#dot").append('svg').attr("height",height).attr("width", width).attr("class", "bg")
+                const svgG = svg.append('g')
+                const svgLabel = svg.append('g');
+                
+                // const svgLine = d3.select("#line").append("svg").attr("width", units).attr("height", yUnits)
+                
+                
         
 
-        
-        
-        
-        
         data.forEach(function (d) {
         
             d.index = +d.index
@@ -106,9 +123,7 @@ const svgLabel = svg.append('g');
             const middle =  data.filter(d=> d.currentTimeStamp == '2019-03-08T14:00:00Z')
             const threeQ =  data.filter(d=> d.currentTimeStamp == '2019-03-03T19:00:00Z')
             const start =  data.filter(d=> d.currentTimeStamp == '2019-03-03T14:00:00Z')
-        
-            console.log(start)
-        
+                
         
         //make an array of eavh indivual time stamp
         //forEach it. with an setTimeout
@@ -118,7 +133,6 @@ const svgLabel = svg.append('g');
         
         const scaleX = d3.scaleLinear().domain([0,45]).range([0,units]);
         // const scaleY = d3.scaleLinear().domain([0,52]).range([0,800]);
-        console.log(scaleX(45))
         
         
         var recs = svgG.selectAll('rect').data(start, d=>d.currentMusher).enter()
@@ -144,8 +158,8 @@ const svgLabel = svg.append('g');
         
         
         const labelDataSelect = labelData.filter((d,i)=> i%2 == 0)
-        console.log('labelDataSelect')
-        console.log(labelDataSelect)
+   
+        
         
         svgLabel.selectAll('text').data(labelDataSelect).enter().append('text')
             .attr("x", textY+leftOffset)
@@ -175,26 +189,21 @@ const svgLabel = svg.append('g');
         // const pete1 = d3.select("#PeterKaiser").attr("fill", "salmon")
         // const nic1 = d3.select("#NicolasPetit").attr("fill", "green")
         // const joar1 = d3.select("#JoarLeifsethUlsom").attr("fill", "blue")
-        // console.log(pete1)
-        // console.log('pete1')
-        // d3.select(“[color=black]”)
+
         
-        
-        // console.log('timesSmall')
-        // console.log(timesSmall)
         
         
         
         function loop () {
         
         
-        
+            console.log(times.length)
+
             times.forEach(function(t, i){
-                console.log(times.length)
         // console.log(i)
                 if (i <= times.length-1) {
-        setTimeout(function(){
-        
+        setTimeout(function inner (){
+        console.log(i)
             // console.log("looping")
             const currentData = data.filter(d=> d.parsedDate == t.toString())
             // console.log(currentData)
@@ -257,8 +266,12 @@ const svgLabel = svg.append('g');
         // const unk = textArea.append('p').text("Unalakleet").attr("class", "city")
         
         
-            },100*i)
-             } else console.log("done??") 
+            },200*i)
+             } else  {
+                 
+                console.log("DDdone??");
+                clearInterval(inner)
+            }
             
             
             
@@ -277,115 +290,107 @@ const svgLabel = svg.append('g');
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        })
-        })
-        
+        //line stuff
 
-        clearInterval()
+
+
+lineData.forEach(function (r){
+
+    r.checkPlace = +r.checkPlace
+    r.index = +r.index
+    // r.musher = r.musher.replace(" ","")
+    r.musherShort = r.musher.replace(" ","").replace(" ","")
+    // r.musherShort = r.musherShort.replace(" ","")
+    
+    // console.log(r)
+    })
+    
+    const musherList = ["Jessie Holmes","Matt Hall","Linwood Fiedler","Mats Pettersson","Peter Kaiser","Jessie Royer","Jason Campeau","Ramey Smyth","Lev Shvarts","Anna Berington","Nicolas Petit","Jeff King","Aliy Zirkle","Martin Buser","Anja Radano","Emily Maxwell","Joar Leifseth Ulsom","Richie Diehl","Kristy Berington","Charley Bejna","Ryan Redington","Ed Hopkins","Brett Bruggeman","Kristin Bacon","Paige Drobny","Mitch Seavey","Aaron Burmeister","Travis Beals","Alison Lifka","Robert Redington","Ryan Santiago","Aaron Peck","Sarah Stokey","Marcelle Fressineau","Jeremy Keller","Sebastien Dos Santos Borges","Cindy Gallea","Richie Beattie","Wade Marrs","Matthew Failor","Martin Apayauq Reitan","Jessica Klejka","Michi Konno","Jeff Deeter","Cindy Abbott","Blair Braverman","Shaynee Traska","Lance Mackey","Seth Barnes","Niklas Wikstrand","Michael Baker","Victoria Hardwick"] 
+    
+    lineData.sort((a,b)=> b.index - a.index)
+        //append svg
+    const svgLine = d3.select("#line").append("svg").attr("width", units).attr("height", yUnits)
+    
+    
+    const pete = lineData.filter(d=> d.musher == "Peter Kaiser")
+    const joar = lineData.filter(d=> d.musher == "Joar Leifseth Ulsom")
+    const aliy = lineData.filter(d=> d.musher == "Aliy Zirkle")
+    // console.log(pete)
+    
+    const keys = d3.keys(lineData)
+    const entries = d3.entries(lineData)
+    // console.log(entries)
+    
+    musherList.forEach(function (d, i){  
+        
+        const mush = lineData.filter(j=> j.musher == d );
+        svgLine.append("path")
+        .datum(mush)
+        .attr("d", lineGenerator)
+        .attr("class", "line")
+        .attr("id", q=>`_${d.replace(" ","").replace(" ","")}`)
+    }
+    )
+    
+    
+    svgLine.append("path")
+        .datum(pete)
+        .attr("d", lineGenerator)
+        .attr("class", "lineSelect")
+    
+        // svgLine.append("path")
+        // .datum(aliy)
+        // .attr("d", lineGenerator)
+        // .attr("class", "lineSelect")
+    
+    
+    // svgLine.append("path")
+    // .datum(joar)
+    // .attr("d", lineGenerator)
+    // .attr("class", "line")
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+} // full
+
+
+full(); 
+        
+        
+        
+        
+        })
+        })  //sedcond csv
+    }) //.linedata csv
+
+
 
 
 ///
 
 
 
-const lineScaleX = d3.scaleLinear().domain([53,0]).range([0,units-10])
-const lineScaleY = d3.scaleLinear().domain([0,23]).range([6,yUnits-35])
-
-const lineGenerator =  d3.line()
-                    .x(d=> lineScaleX(d.checkPlace))
-                    .y(d=>lineScaleY(d.index))
-                    .curve(d3.curveNatural) 
-                    .curve(d3.curveBundle) 
-                    .curve(d3.curveStep) 
-
-d3.csv("data/resGroup.csv", function (lineData){
-
-
-
-lineData.forEach(function (r){
-
-r.checkPlace = +r.checkPlace
-r.index = +r.index
-// r.musher = r.musher.replace(" ","")
-r.musherShort = r.musher.replace(" ","").replace(" ","")
-// r.musherShort = r.musherShort.replace(" ","")
-
-// console.log(r)
-})
-
-const musherList = ["Jessie Holmes","Matt Hall","Linwood Fiedler","Mats Pettersson","Peter Kaiser","Jessie Royer","Jason Campeau","Ramey Smyth","Lev Shvarts","Anna Berington","Nicolas Petit","Jeff King","Aliy Zirkle","Martin Buser","Anja Radano","Emily Maxwell","Joar Leifseth Ulsom","Richie Diehl","Kristy Berington","Charley Bejna","Ryan Redington","Ed Hopkins","Brett Bruggeman","Kristin Bacon","Paige Drobny","Mitch Seavey","Aaron Burmeister","Travis Beals","Alison Lifka","Robert Redington","Ryan Santiago","Aaron Peck","Sarah Stokey","Marcelle Fressineau","Jeremy Keller","Sebastien Dos Santos Borges","Cindy Gallea","Richie Beattie","Wade Marrs","Matthew Failor","Martin Apayauq Reitan","Jessica Klejka","Michi Konno","Jeff Deeter","Cindy Abbott","Blair Braverman","Shaynee Traska","Lance Mackey","Seth Barnes","Niklas Wikstrand","Michael Baker","Victoria Hardwick"] 
-
-lineData.sort((a,b)=> b.index - a.index)
-    //append svg
-const svgLine = d3.select("#line").append("svg").attr("width", units).attr("height", yUnits)
-
-
-const pete = lineData.filter(d=> d.musher == "Peter Kaiser")
-const joar = lineData.filter(d=> d.musher == "Joar Leifseth Ulsom")
-const aliy = lineData.filter(d=> d.musher == "Aliy Zirkle")
-console.log(pete)
-
-const keys = d3.keys(lineData)
-const entries = d3.entries(lineData)
-console.log(entries)
-
-musherList.forEach(function (d, i){  
-    
-    const mush = lineData.filter(j=> j.musher == d );
-    svgLine.append("path")
-    .datum(mush)
-    .attr("d", lineGenerator)
-    .attr("class", "line")
-    .attr("id", q=>`_${d.replace(" ","").replace(" ","")}`)
-}
-)
-
-
-svgLine.append("path")
-    .datum(pete)
-    .attr("d", lineGenerator)
-    .attr("class", "lineSelect")
-
-    // svgLine.append("path")
-    // .datum(aliy)
-    // .attr("d", lineGenerator)
-    // .attr("class", "lineSelect")
-
-
-// svgLine.append("path")
-// .datum(joar)
-// .attr("d", lineGenerator)
-// .attr("class", "line")
-
-}) //.csv
 
 
 
 
-}
-
-
-full();
 
 
 
